40691a15d19592b91aba30405b9f11869e7fa36a
emizuray <lavrauw.unipr@gmail.com>
2025-08-27 12:45:33 +0200

    removed spaces in the file name

---
+++ lib/RepO15.gd
@@ -0,0 +1,19 @@
+﻿
+#############################################################################
+##
+##  RepO15.gd              T233 package
+##                                                            Nour Alnajjarine
+##                                                            Michel Lavrauw
+##                                                        
+##
+##  Copyright 2019	Sabanci University
+##                  
+##  Declaration stuff for representative of the orbit o15
+##
+#############################################################################
+
+
+
+DeclareGlobalFunction( "RepO15odd" );
+DeclareGlobalFunction( "RepO15even" );
+
